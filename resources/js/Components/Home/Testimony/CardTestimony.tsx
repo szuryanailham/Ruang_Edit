b792a49f6c24b0d11a81417c8b7417b9f37245a2
ilham Suryana <szuryanailham090102@gmail.com>
2024-06-18 21:45:23 +0700

    menambah testimony dan sitingan ukuran card testimony

--- resources/js/Components/Home/Testimony/CardTestimony.tsx
+++ resources/js/Components/Home/Testimony/CardTestimony.tsx
@@ -6,7 +6,12 @@ import {
 } from "@/Components/shadcn/ui/avatar";
 import { FaStar } from "react-icons/fa6";
 
-const CardTestimony: React.FC = () => {
+interface CardTestimonyProps {
+    name: string;
+    comment: string;
+}
+
+const CardTestimony: React.FC<CardTestimonyProps> = ({ name, comment }) => {
     const stars = () => {
         const array = [...Array(5)];
         return (
@@ -21,7 +26,7 @@ const CardTestimony: React.FC = () => {
         );
     };
     return (
-        <div className="bg-white w-[300px] md:w-[500px] lg:w-[350px] h-[150px] md:h-[220px] lh:h-[200px] p-1 rounded-lg shadow-md">
+        <div className="bg-white w-[300px] md:w-[500px] lg:w-[350px] h-fit md:h-[220px] lh:h-[200px] p-1 rounded-lg shadow-md">
             <div className="flex item-center py-2 px-3 gap-5 md:gap-7">
                 <Avatar className="w-[60px] md:w-[70px] lg:w-[50px] h-[60px] md:h-[70px] lg:h-[50px]">
                     <AvatarImage src="https://github.com/shadcn.png" />
@@ -30,15 +35,14 @@ const CardTestimony: React.FC = () => {
                 {/* name and rating */}
                 <div>
                     {/* Nama  */}
-                    <h1 className="text-xl font-bold">Ilham Suryana</h1>
+                    <h1 className="text-xl font-bold">{name}</h1>
                     {/* Rating */}
                     <div className="mt-2">{stars()}</div>
                 </div>
                 {/* comment */}
             </div>
-            <p className=" px-3 mb-3 leading-5 text-center text-xs md:text-xl lg:text-sm md:px-10 lg:mt-5">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
-                ipsa fugit! Placeat aperiam
+            <p className=" px-3 mb-3  text-center text-xs md:text-xl lg:text-sm md:px-10 lg:mt-5 max-w-prose leading-4 md:leading-5">
+                {comment}
             </p>
         </div>
     );
