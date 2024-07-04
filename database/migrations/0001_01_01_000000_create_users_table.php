130d0509b4b77aa2c3f591e1f5504822b51f046f
ilham Suryana <szuryanailham090102@gmail.com>
2024-07-05 00:04:16 +0700

    fix the problem of authentication 2

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->text('alamat')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->string('google_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
